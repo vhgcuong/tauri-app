2acb503a6e85d040b53c498405bb1f167b479de8
cuong.vuhung <cuong.vuhung@vti.com.vn>
2025-01-06 17:00:18 +0700

    #1 Add component count

--- show-info/src/App.vue
+++ show-info/src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import Greet from "./components/greet.vue";
 import Welcome from "./components/welcome.vue";
+import Count from "./components/count.vue";
 </script>
 
 <template>
   <main class="mt-8 container mx-auto text-center">
     <welcome />
     <greet />
+    <count />
   </main>
 </template>
 
---
+++ show-info/src/components/count.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="h-full flex justify-center items-center">
+    <button class="bg-gradient-to-r from-blue-300 to-blue-800" @click="count++">
+      Count is: {{ count }}
+    </button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
